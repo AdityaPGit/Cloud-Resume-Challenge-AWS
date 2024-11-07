fc40f60c26c5158db940ae22a8494cdcd50f4414
AdityaP <134126030+AdityaPGit@users.noreply.github.com>
2024-11-07 16:41:02 +0530

    Entire code pushing

--- IaC/outputs.tf
+++ IaC/outputs.tf
@@ -2,3 +2,7 @@ output "cloudfront-url" {
   value = "https://${aws_cloudfront_distribution.website-distribution.domain_name}"
   description = "URL for cloudfront distribution"
 }
+output "apigateway-url" {
+  value = aws_apigatewayv2_stage.counter-api-deployment-stage.invoke_url
+  description = "URL for the apigateway associated"
+}
--- website/index.js
+++ website/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(){
     const counter = document.querySelector(".counter");
     async function updateCounter(){
         try {
-            let response = await fetch("")
+            let response = await fetch("https://a8fu2b1awh.execute-api.ap-south-1.amazonaws.com/count")
             let data = await response.json();
             console.log('Count:', data);
             counter.innerHTML = data;
